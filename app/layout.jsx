e6ec754af02f6ebb98409930f161483c0d7b7689
AyjayJr <ayjayjr@hotmail.com>
2023-07-28 19:22:13 -0400

    setting up blank next project

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+
+export const metadata = {
+	title: "Anthony Jr | Portfolio",
+	description: "Anthony Jr's portfolio showcasing skills and related works"
+}
+
+const layout = ({ children }) => {
+	return (
+		<html lang="en">
+			<body>
+				{children}
+			</body>
+		</html>
+	)
+}
+
+export default layout
