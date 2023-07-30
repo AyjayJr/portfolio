36110dc38c25041c70a313f0005c4796daee8229
AyjayJr <ayjayjr@hotmail.com>
2023-07-30 15:27:15 -0400

    added footer

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero"
 import About from "@/components/About"
 import Projects from "@/components/Projects"
 import Contact from "@/components/Contact"
+import Footer from "@/components/Footer"
 
 const Home = () => {
 	return (
@@ -10,6 +11,7 @@ const Home = () => {
 			<About />
 			<Projects />
 			<Contact />
+			<Footer />
 		</div>
 	)
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+
+const Footer = () => {
+	return (
+		<div className="py-10 flex flex-col items-center">
+			<strong>Copyright &#169; 2023</strong>
+			<div className="pt-5">
+				<a target="_blank" href="https://linkedin.com/in/anthony-j-garcia-jr">
+					<FontAwesomeIcon
+						className="inline ml-1 mr-4 hover:scale-125 transition ease-in-out"
+						icon={faLinkedin}
+						width={25}
+					/>
+				</a>
+				<a target="_blank" href="https://github.com/ayjayjr">
+					<FontAwesomeIcon
+						className="inline hover:scale-125 transition ease-in-out"
+						icon={faGithub}
+						width={25}
+					/>
+				</a>
+			</div>
+		</div>
+	)
+}
+
+export default Footer
