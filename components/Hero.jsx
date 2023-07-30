27235fc2aa347c24f7545ac8480e6842c7b5d937
AyjayJr <ayjayjr@hotmail.com>
2023-07-29 23:23:53 -0400

    added divs around tech icons

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 					<h1 className="text-6xl gradient_text pb-8">
 						<b>Full-stack Developer</b>
 					</h1>
-					<p className="ml-1 mb-8 text-lg max-w-xl">
+					<p className="ml-1 mb-8 text-xl max-w-xl">
 						Hi, I'm <strong>Anthony Garcia Jr.</strong> A passionate full-stack developer based out of <strong>Florida, USA </strong>👋🏽
 					</p>
 					<a target="_blank" href="https://linkedin.com/in/anthony-j-garcia-jr">
@@ -31,64 +31,76 @@ const Hero = () => {
 				</div>
 				<div
 					className="mb-10 bg-hero-img w-96 h-96 bg- bg-cover bg-center bg-no-repeat shadow-md border-gray-200 border-2 border_animation"
-					priority="true" 	
+					priority="true"
 				>
 				</div>
 			</div>
-			<div className="flex items-center justify-center lg:justify-start gap-4">
+			<div className="flex flex-wrap items-center justify-center lg:justify-start gap-4">
 				<h2 className="text-lg">
 					<strong className="hidden lg:block text-text">
 						Tech Stack
 					</strong>
 				</h2>
+				<div className="floating shadow-xl rounded-full border border-gray-200">
+					<Image
+						className="inline m-4"
+						src="/assets/html-5.png"
+						alt="html5_logo"
+						width={40}
+						height={40}
+						unoptimized
+					/>
+				</div>
+				<div className="floating delay_2s shadow-xl rounded-full border border-gray-200">
 				<Image
-					className="inline floating"
-					src="/assets/html-5.png"
-					alt="html5_logo"
-					width={50}
-					height={50}
-					unoptimized
-				/>
-				<Image
-					className="inline floating delay_2s"
+					className="inline m-4"
 					src="/assets/css-3.png"
 					alt="css3_logo"
-					width={50}
-					height={50}
+					width={40}
+					height={40}
 					unoptimized
 				/>
+				</div>
+				<div className="floating shadow-xl rounded-full border border-gray-200">
 				<Image
-					className="inline floating"
+					className="inline m-4"
 					src="/assets/js.png"
 					alt="js_logo"
-					width={50}
-					height={50}
+					width={40}
+					height={40}
 					unoptimized
 				/>
+				</div>
+				<div className="floating shadow-xl rounded-full border border-gray-200">
 				<Image
-					className="inline floating delay_2s"
+					className="inline m-4"
 					src="/assets/reactjs.svg"
 					alt="reactjs_logo"
-					width={50}
-					height={50}
+					width={40}
+					height={40}
 					unoptimized
 				/>
+				</div>
+				<div className="floating delay_2s shadow-xl rounded-full border border-gray-200">
 				<Image
-					className="inline floating"
+					className="inline m-4"
 					src="/assets/nextjs.svg"
 					alt="nextjs_logo"
-					width={50}
-					height={50}
+					width={40}
+					height={40}
 					unoptimized
 				/>
+				</div>
+				<div className="floating shadow-xl rounded-full border border-gray-200">
 				<Image
-					className="inline floating delay_2s"
+					className="inline m-4"
 					src="/assets/tailwind.svg"
 					alt="tailwind_logo"
-					width={50}
-					height={50}
+					width={40}
+					height={40}
 					unoptimized
 				/>
+				</div>
 			</div>
 		</div>
 	)
