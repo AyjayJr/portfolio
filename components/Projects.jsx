014f69e12ade2fd13b0e7618a3a74de5ce71303a
AyjayJr <48783536+AyjayJr@users.noreply.github.com>
2023-08-17 12:48:28 -0400

    made project section accessible on smaller devices

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,51 +4,26 @@ import { motion } from "framer-motion"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedI } from "@fortawesome/free-brands-svg-icons";
 
-const cardVariants = {
-	offscreen: {
-		y: 300
-	},
-	onscreen: {
-		y: 50,
-		rotate: -10,
-		transition: {
-			type: "spring",
-			bounce: 0.4,
-			duration: 0.8
-		}
-	}
-};
-
-function Card({ emoji, hueA, hueB }) {
-	return (
-		<motion.div
-			className="card_container"
-			initial="offscreen"
-			whileInView="onscreen"
-			viewport={{ once: true, amount: 0.8 }}
-		>
-			<motion.div className="card" variants={cardVariants}>
-				{emoji}
-			</motion.div>
-			<motion.div
-				className="max-w-sm mx-auto text-xl"
-			>
-				<p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, repudiandae dolorem! Dolorem placeat, eum accusantium eos, recusandae quo rem nostrum molestiae quod similique corrupti aperiam, velit soluta voluptatem omnis? Nihil!</p>
-			</motion.div>
-		</motion.div>
-	);
-}
-
-const food = [
-	["🍅", 340, 10],
-	["🍊", 20, 40],
-	["🍋", 60, 90],
-	["🍐", 80, 120],
-];
 
 const Projects = ({ id }) => {
+
+  const cardVariants = {
+    offscreen: {
+      y: 300
+    },
+    onscreen: {
+      y: 50,
+      rotate: -10,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+      }
+    }
+  }
+
 	return (
-    <div id={id} className="text-center lg:text-left py-44 mx-5 border-b-2 border-gray-200">
+    <div className="flex flex-col text-center lg:text-left py-44 mx-5 border-b-2 border-gray-200">
 			<h2 className="text-xl text-accent lg:text-left">
 				<strong>
 					PROJECTS
@@ -59,8 +34,21 @@ const Projects = ({ id }) => {
 					Each project is a unique piece of development 🧩
 				</strong>
 			</p>
-			{food.map(([emoji, hueA, hueB]) => (
-				<Card emoji={emoji} hueA={hueA} hueB={hueB} key={emoji} />))}
+      <motion.div
+        className="card_container flex flex-col lg:flex-row"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <motion.div className="card mb-16" variants={cardVariants}>
+          A 
+        </motion.div>
+        <motion.div
+          className="max-w-sm mx-auto text-xl"
+        >
+          <motion.p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, repudiandae dolorem! Dolorem placeat, eum accusantium eos, recusandae quo rem nostrum molestiae quod similique corrupti aperiam, velit soluta voluptatem omnis? Nihil!</motion.p>
+        </motion.div>
+      </motion.div>
 		</div>
 	)
 }
