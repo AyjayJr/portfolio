5a7730c6e1652d6da0632705f15a8329c443e609
AyjayJr <48783536+AyjayJr@users.noreply.github.com>
2023-08-16 21:29:50 -0400

    hero section style changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,16 +3,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
+const TechIcon = ({ src }) => {
+  return (
+    <div className="ml-8 shadow-xl rounded-full border border-gray-200">
+      <Image
+        className="inline m-4"
+        src={src}
+        alt="logo"
+        width={40}
+        height={40}
+        unoptimized
+      />
+    </div>
+  )
+}
+
 const Hero = ({ id }) => {
+
+  const images = [
+    "/assets/html-5.png",
+    "/assets/css-3.png",
+    "/assets/js.png",
+    "/assets/reactjs.svg",
+    "/assets/nextjs.svg",
+    "/assets/tailwind.svg"
+  ]
+
 	return (
 		<div id={id} className="pb-56 border-b-2 border-gray-200">
 			<div className="pb-10 mb-10 flex flex-col-reverse lg:flex-row lg:text-left text-center items-center justify-between">
 				<div className="max-w-2xl">
 					<h1 className="text-6xl pb-8">
-						<b>Full-stack Developer</b>
+						<b>Software Engineer</b>
 					</h1>
 					<p className="ml-1 mb-8 text-xl max-w-xl">
-						Hi, I'm <strong>Anthony Garcia Jr.</strong> A dedicated full-stack developer based out of <strong>Florida, USA </strong>👋🏽
+						Hi, I'm <strong>Anthony Garcia Jr.</strong> a recent Computer Science graduate with a strong enthusiam for software engineering and a hunger for continuous learning.
 					</p>
 					<a target="_blank" href="https://linkedin.com/in/anthony-j-garcia-jr">
 						<FontAwesomeIcon
@@ -30,9 +55,14 @@ const Hero = ({ id }) => {
 					</a>
 				</div>
 				<div
-					className="mb-10 bg-hero-img w-96 h-96 bg-cover bg-center bg-no-repeat shadow-md rounded-full relative glowing_gradient border_animation"
+					className="relative mb-10 bg-hero-img w-96 h-96 bg-cover bg-center bg-no-repeat shadow-md rounded-full relative glowing_gradient" 
 					priority="true"
 				>
+          <div
+            className="border border-gray-200 drop-shadow-md p-3 cursor-default bg-gray-100 rounded-full absolute bottom-10 left-0 text-4xl" 
+          >
+            👋
+          </div>
 				</div>
 			</div>
 			<div className="flex flex-wrap items-center justify-center lg:justify-start gap-4">
@@ -41,66 +71,7 @@ const Hero = ({ id }) => {
 						Tech Stack
 					</strong>
 				</h2>
-				<div className="floating shadow-xl rounded-full border border-gray-200">
-					<Image
-						className="inline m-4"
-						src="/assets/html-5.png"
-						alt="html5_logo"
-						width={40}
-						height={40}
-						unoptimized
-					/>
-				</div>
-				<div className="floating delay_2s shadow-xl rounded-full border border-gray-200">
-					<Image
-						className="inline m-4"
-						src="/assets/css-3.png"
-						alt="css3_logo"
-						width={40}
-						height={40}
-						unoptimized
-					/>
-				</div>
-				<div className="floating shadow-xl rounded-full border border-gray-200">
-					<Image
-						className="inline m-4"
-						src="/assets/js.png"
-						alt="js_logo"
-						width={40}
-						height={40}
-						unoptimized
-					/>
-				</div>
-				<div className="floating delay_2s shadow-xl rounded-full border border-gray-200">
-					<Image
-						className="inline m-4"
-						src="/assets/reactjs.svg"
-						alt="reactjs_logo"
-						width={40}
-						height={40}
-						unoptimized
-					/>
-				</div>
-				<div className="floating shadow-xl rounded-full border border-gray-200">
-					<Image
-						className="inline m-4"
-						src="/assets/nextjs.svg"
-						alt="nextjs_logo"
-						width={40}
-						height={40}
-						unoptimized
-					/>
-				</div>
-				<div className="floating delay_2s shadow-xl rounded-full border border-gray-200">
-					<Image
-						className="inline m-4"
-						src="/assets/tailwind.svg"
-						alt="tailwind_logo"
-						width={40}
-						height={40}
-						unoptimized
-					/>
-				</div>
+        {images.map((image) => <TechIcon src={image} />)}
 			</div>
 		</div>
 	)
