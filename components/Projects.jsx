a123dea09c19c4c709dc549b49a4ea5ca46b5576
AyjayJr <48783536+AyjayJr@users.noreply.github.com>
2023-08-17 14:47:05 -0400

    completed foodera project section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,8 +2,9 @@
 
 import Image from "next/image"
 import { motion } from "framer-motion"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedI } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faLink } from "@fortawesome/free-solid-svg-icons"
 
 
 const Projects = ({ id }) => {
@@ -27,17 +28,17 @@ const Projects = ({ id }) => {
     <div className="flex flex-col text-center lg:text-left py-44 mx-5 border-b-2 border-gray-200">
 			<h2 className="text-xl text-accent lg:text-left">
 				<strong>
-					PROJECTS
+				  PORTFOLIO	
 				</strong>
 			</h2>
 			<p className="my-4 text-2xl text-text">
 				<strong>
-					Each project is a unique piece of development 🧩
+          Each project showcases a unique skillset ⚙ 
 				</strong>
 			</p>
 
       <motion.div
-        className="card_container flex flex-col lg:flex-row"
+        className="card_container flex align-start flex-col lg:flex-row"
         initial="offscreen"
         whileInView="onscreen"
         viewport={{ once: true, amount: 0.8 }}
@@ -54,7 +55,28 @@ const Projects = ({ id }) => {
         <motion.div
           className="max-w-sm mx-auto text-xl"
         >
-          <motion.p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, repudiandae dolorem! Dolorem placeat, eum accusantium eos, recusandae quo rem nostrum molestiae quod similique corrupti aperiam, velit soluta voluptatem omnis? Nihil!</motion.p>
+          <h2 className="text-center mb-4"><strong>Foodera</strong></h2>
+          <motion.p className="text-center text-gray-500">
+            A website showcasing Foodera a deli/bakery that provides good alternatives to fast food. The website incorporates a responsive user-friendly design that can be viewed comfortably on all screen sizes.
+          </motion.p>
+          <div className="w-full flex justify-center gap-10 mt-6">
+            <a
+              className="border p-3 active:shadow-sm shadow-lg border-gray-300 rounded"
+              target="_blank"
+              href="https://github.com/AyjayJr/foodera"
+            >
+              <FontAwesomeIcon className="mr-2" icon={faGithub} />
+              Source Code
+            </a>
+            <a
+              className="border p-3 active:shadow-sm shadow-lg border-gray-300 rounded"
+              target="_blank"
+              href="https://foodera-landing-page.vercel.app/"
+            >
+              <FontAwesomeIcon className="mr-2" icon={faLink} />
+              Live Demo
+            </a>
+          </div>
         </motion.div>
       </motion.div>
 
