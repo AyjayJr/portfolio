6e23fe02d1439d5a5bf40f446983300abfca4f8e
AyjayJr <48783536+AyjayJr@users.noreply.github.com>
2023-08-19 16:20:44 -0400

    trying to fix build error

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,14 @@
-"use client"
-
 import "@/styles/globals.css"
 import "@fortawesome/fontawesome-svg-core/styles.css"
+
+export const metadata = {
+	title: "Anthony Garcia",
+	description: "Showcasing the skills and related projects of Anthony Garcia Jr"
+}
 
 const layout = ({ children }) => {
 	return (
 		<html lang="en">
-      <meta type="title" content="Anthony Garcia" />
-      <meta type="description" content="Showcasing the skills and related projects of Anthony Garcia Jr" />
 			<body
         id="portal"
         className="bg-bg max-w-screen container mt-48 mx-auto max-w-5xl"
--- app/page.jsx
+++ app/page.jsx
@@ -8,28 +8,30 @@ import Projects from "@/components/Projects"
 import Contact from "@/components/Contact"
 import Footer from "@/components/Footer"
 
-import { useState } from "react"
-import ReactDOM, { createPortal } from "react-dom"
+import { useState, useEffect } from "react"
+import { createPortal } from "react-dom"
 import { config } from "@fortawesome/fontawesome-svg-core"
 
 config.autoAddCss = false;
 
 const Home = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
   
   const openMenu = () => { setIsMenuOpen(prev => !prev)} 
 
-  const portalRoot = document.getElementById("portalRoot")
 
 	return (
 		<>
       <header className="dark:bg-dark_bg dark:shadow-accent bg-bg py-6 fixed top-0 left-0 right-0 shadow-lg z-10">
 				<Nav clickHandler={openMenu} />
 			</header>
-      {createPortal(
+      {mounted ? createPortal(
         <ModalMenu menuOpen={isMenuOpen} clickHandler={openMenu} />,
-        portalRoot
-      )}
+        document.body 
+      ) : null}
 			<main>
 				<Hero id="#" />
 				<About id="about" />
